392e36fca06ac48b5db6cf5e3d32f61f96b4d697
n4oah <n4oahdev@gmail.com>
2024-06-28 00:03:08 +0900

    initial commit

---
+++ src/main/java/com/fade/FadeApplication.java
@@ -0,0 +1,13 @@
+package com.fade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fade/FadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
