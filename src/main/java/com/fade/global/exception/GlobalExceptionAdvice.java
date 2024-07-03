635b05c01229bade6d5008430c8d8d37b9157856
Junobee25 <wnsdhqo@naver.com>
2024-07-03 23:59:02 +0900

    feat:#2 커스텀 예외 클래스, 전역 Advice 정의

---
+++ src/main/java/com/fade/global/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,8 @@
+package com.fade.global.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
+}
