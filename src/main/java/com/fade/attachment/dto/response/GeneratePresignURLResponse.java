d36f0f736f584da8cd1c63cdb65f996482eb0fea
n4oah <n4oahdev@gmail.com>
2024-07-09 01:27:18 +0900

    feat: attachmentId 추가

--- src/main/java/com/fade/attachment/dto/response/GeneratePresignURLResponse.java
+++ src/main/java/com/fade/attachment/dto/response/GeneratePresignURLResponse.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record GeneratePresignURLResponse(
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "5분간 업로드 권한 유지")
-        String presignURL
+        String presignURL,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "파일 고유 키")
+        Long attachmentId
 ) {
 
 }
