1c5f4ce84edea963d3f6f1dcc2d4d0a4ab40ac8f
Junobee25 <wnsdhqo@naver.com>
2024-07-11 14:26:40 +0900

    feat: #18 투표 레포지토리 정의

---
+++ src/main/java/com/fade/vote/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.fade.vote.repository;
+
+import com.fade.vote.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
