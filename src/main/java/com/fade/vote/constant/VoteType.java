f67208a64cc73f2dedb581e2f1c7ae0f7aa71047
Junobee25 <wnsdhqo@naver.com>
2024-07-11 18:48:57 +0900

    feat: #18 투표 타입 파라미터 검증 추가, ExceptionHandler 적용

--- src/main/java/com/fade/vote/constant/VoteType.java
+++ src/main/java/com/fade/vote/constant/VoteType.java
@@ -1,6 +1,18 @@
 package com.fade.vote.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.stream.Stream;
+
 public enum VoteType {
     FADE_IN,
-    FADE_OUT
+    FADE_OUT;
+
+    @JsonCreator
+    public static VoteType parsing(String inputValue) {
+        return Stream.of(VoteType.values())
+                .filter(voteType -> voteType.toString().equals(inputValue.toUpperCase()))
+                .findAny()
+                .orElse(null);
+    }
 }
