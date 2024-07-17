a3bd990fb68efeecf5a16f64decc36a399af7982
n4oah <n4oahdev@gmail.com>
2024-07-17 20:51:42 +0900

    fix: reuqest body formData

--- src/main/java/com/fade/global/component/impl/KakaoOAuth2Provider.java
+++ src/main/java/com/fade/global/component/impl/KakaoOAuth2Provider.java
@@ -3,6 +3,7 @@ package com.fade.global.component.impl;
 import com.fade.global.component.OAuth2Provider;
 import com.fade.global.dto.OAuthProfile;
 import com.fade.sociallogin.constant.SocialType;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -20,26 +22,27 @@ import java.util.Objects;
 public class KakaoOAuth2Provider implements OAuth2Provider {
     private final RestTemplate restTemplate;
 
-    @Value("kakao.oauth.client-id")
+    @Value("${kakao.oauth.client-id}")
     private String clientId;
-    @Value("kakao.oauth.client-secret")
+    @Value("${kakao.oauth.client-secret}")
     private String clientSecret;
-    @Value("kakao.oauth.token-url")
+    @Value("${kakao.oauth.token-url}")
     private String tokenUrl;
-    @Value("kakao.oauth.profile-url")
+    @Value("${kakao.oauth.profile-url}")
     private String profileUrl;
 
     @Override
     public String getAccessToken(String code, String redirectUri) {
-        final var requestBody = Map.of(
-                "grant_type", "authorization_code",
-                "client_id", this.clientId,
-                "redirect_uri", redirectUri,
-                "code", code,
-                "client_secret", this.clientSecret
-        );
+        final var requestBody = new LinkedMultiValueMap<String, Object>();
+
+        requestBody.set("grant_type", "authorization_code");
+        requestBody.set("client_id", this.clientId);
+        requestBody.set("redirect_uri", redirectUri);
+        requestBody.set("code", code);
+        requestBody.set("client_secret", this.clientSecret);
+
         final var headers = new HttpHeaders();
-        headers.set("Content-type", "Content-type: application/x-www-form-urlencoded;charset=utf-8");
+        headers.set("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         final var httpEntity = new HttpEntity<>(requestBody, headers);
 
@@ -47,16 +50,16 @@ public class KakaoOAuth2Provider implements OAuth2Provider {
                 this.tokenUrl,
                 HttpMethod.POST,
                 httpEntity,
-                GetAccessTokenResponse.class
+                Map.class
         );
 
-        return Objects.requireNonNull(response.getBody()).getAccess_token();
+        return Objects.requireNonNull((String) Objects.requireNonNull(response.getBody()).get("access_token"));
     }
 
     @Override
     public OAuthProfile getProfile(String accessToken) {
         final var headers = new HttpHeaders();
-        headers.set("Content-type", "Content-type: application/x-www-form-urlencoded;charset=utf-8");
+        headers.set("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
         headers.set("Authorization", "Bearer " + accessToken);
 
         final var httpEntity = new HttpEntity<>(headers);
@@ -73,11 +76,5 @@ public class KakaoOAuth2Provider implements OAuth2Provider {
             response.getBody(),
             SocialType.KAKAO
         );
-    }
-
-    @RequiredArgsConstructor
-    @Getter
-    private static class GetAccessTokenResponse {
-        private final String access_token;
     }
 }
