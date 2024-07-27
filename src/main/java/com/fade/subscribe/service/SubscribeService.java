9583addbebfbf1c0e0be1afee3df67f8ce756f33
n4oah <n4oahdev@gmail.com>
2024-07-27 19:15:38 +0900

    feat: subscirbe controller에 feed list api 삭제

--- src/main/java/com/fade/subscribe/service/SubscribeService.java
+++ src/main/java/com/fade/subscribe/service/SubscribeService.java
@@ -3,12 +3,9 @@ package com.fade.subscribe.service;
 import com.fade.attachment.constant.AttachmentLinkType;
 import com.fade.attachment.constant.AttachmentLinkableType;
 import com.fade.attachment.service.AttachmentService;
-import com.fade.category.dto.response.FindCategoryListResponse;
 import com.fade.faparchiving.repository.FapArchivingRepository;
-import com.fade.feed.repository.FeedRepository;
 import com.fade.member.entity.Member;
 import com.fade.member.service.MemberCommonService;
-import com.fade.feed.dto.response.FindSubscribeFeedResponse;
 import com.fade.subscribe.dto.response.FindSubscriberResponse;
 import com.fade.subscribe.entity.Subscribe;
 import com.fade.subscribe.repository.SubscribeRepository;
@@ -22,7 +19,6 @@ public class SubscribeService {
     private final MemberCommonService memberCommonService;
     private final SubscribeRepository subscribeRepository;
     private final SubscribeCommonService subscribeCommonService;
-    private final FeedRepository feedRepository;
     private final FapArchivingRepository fapArchivingRepository;
     private final AttachmentService attachmentService;
 
@@ -46,44 +42,6 @@ public class SubscribeService {
         Subscribe subscribe = subscribeCommonService.findByFromMemberIdAndToMemberId(fromMemberId, toMemberId);
 
         subscribeRepository.delete(subscribe);
-    }
-
-    @Transactional(readOnly = true)
-    public FindSubscribeFeedResponse findSubscribeFeeds(Long fromMemberId, Long nextCursor, int limit) {
-        final var member = memberCommonService.findById(fromMemberId);
-        final var subscribes = subscribeRepository.findByFromMemberId(member.getId());
-        final var subscribeMemberIds = subscribes.stream()
-                .map(subscribe -> subscribe.getToMember().getId())
-                .toList();
-//        final var feeds = feedRepository.findFeedsByMemberIds(subscribeMemberIds, nextCursor, limit);
-//
-//        return new FindSubscribeFeedResponse(
-//                feeds.stream().map(feed -> new FindSubscribeFeedResponse.FindSubscribeFeedItemResponse(
-//                        feed.getId(),
-//                        this.attachmentService.getUrl(
-//                                feed.getId(),
-//                                AttachmentLinkableType.FEED,
-//                                AttachmentLinkType.IMAGE
-//                        ),
-//                        feed.getStyles().stream().map(style -> new FindSubscribeFeedResponse.FindSubscribeFeedStyleResponse(
-//                                style.getId(),
-//                                style.getName()
-//                        )).toList(),
-//                        feed.getFeedOutfitList().stream().map(feedOutfit -> new FindSubscribeFeedResponse.FindSubscribeFeedOutfitResponse(
-//                                feedOutfit.getId(),
-//                                feedOutfit.getBrandName(),
-//                                feedOutfit.getProductName(),
-//                                new FindCategoryListResponse.FindCategoryItemResponse(
-//                                        feedOutfit.getCategory().getId(),
-//                                        feedOutfit.getCategory().getName()
-//                                )
-//                        )).toList(),
-//                        feed.getMember().getId(),
-//                        isArchiving(feed.getId())
-//                )).toList(),
-//                !feeds.isEmpty() ? feeds.get(feeds.size() - 1).getId() : null
-//        );
-        return null;
     }
 
     @Transactional(readOnly = true)
