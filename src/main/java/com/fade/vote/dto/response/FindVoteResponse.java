cfe37428c1eb7837b045288d618588c496bde119
Junobee25 <wnsdhqo@naver.com>
2024-07-28 03:33:30 +0900

    refactor: #74 투표,구독,알림,페이피 dto 명명 및 구조 변경 이에 대응하는 코드 변경

--- src/main/java/com/fade/vote/dto/response/FindVoteResponse.java
+++ src/main/java/com/fade/vote/dto/response/FindVoteResponse.java
@@ -25,7 +25,7 @@ public record FindVoteResponse(
             String feedImageURL,
             Boolean isFapFeed,
             List<FindVoteItemStyleResponse> stylesId,
-            List<FindVoteItemOutFitResponse> outFits,
+            List<FindVoteItemOutFitResponse> outfits,
             String username,
             String profileImageURL
     ) {
