95f792ca55df907adcb0901deab73af99ed04a98
Junobee25 <wnsdhqo@naver.com>
2024-08-13 03:43:11 +0900

    fix: 탈퇴한 계정 재로그인 시 탈퇴 처리 구현

--- src/main/java/com/fade/global/constant/ErrorCode.java
+++ src/main/java/com/fade/global/constant/ErrorCode.java
@@ -21,6 +21,7 @@ public enum ErrorCode {
     ALREADY_EXIST_MEMBER_ID(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다."),
     ALREADY_EXIST_MEMBER(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
     INVALID_MEMBER_ID_AND_PASSWORD(HttpStatus.BAD_REQUEST, "아이디 혹은 비밀번호를 확인해주세요."),
+    SIGN_IN_WITH_RESIGNED_MEMBER(HttpStatus.FORBIDDEN, "탈퇴한 계정입니다."),
 
     //FEED
     NOT_FOUND_FEED(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
