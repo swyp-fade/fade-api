a254e6cb921c24991dd7a606bb4500f00df2221e
Junobee25 <wnsdhqo@naver.com>
2024-09-23 15:20:34 +0900

    fix: Enum 필드에 대한 사용법은 @NotNull

--- src/main/java/com/fade/bon/dto/request/VoteBonReq.java
+++ src/main/java/com/fade/bon/dto/request/VoteBonReq.java
@@ -3,10 +3,11 @@ package com.fade.bon.dto.request;
 import com.fade.bon.constant.BonVoteType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record VoteBonReq(
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotEmpty
+        @NotNull
         BonVoteType bonVoteType
 ) {
 }
